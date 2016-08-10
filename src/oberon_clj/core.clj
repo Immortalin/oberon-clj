46b66b57174493c846b005a11243e9810a5a9429
Immortalin <immortalin@icloud.com>
2016-08-10 21:35:06 +0800

    updated stuff

--- src/oberon_clj/core.clj
+++ src/oberon_clj/core.clj
@@ -7,6 +7,23 @@
 (def oberon-syntax
   (instaparse/parser (slurp "./resources/oberon-07.ebnf")))
 
+(def test-oberon-syntax []
+  (oberon-syntax
+   "MODULE Hello;
+  IMPORT Oberon, Texts;
+  VAR W: Texts.Writer;
+
+  PROCEDURE World*;
+  BEGIN
+    Texts.WriteString(W, \"Hello World!\");
+    Texts.WriteLn(W);
+    Texts.Append(Oberon.Log, W.buf);
+  END World;
+
+BEGIN
+  Texts.OpenWriter(W);
+END Hello
+"))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
